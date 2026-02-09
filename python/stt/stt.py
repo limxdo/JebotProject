5fa36175b3e09a63c7866162f2c697423471b46d
killer42-byte <zyadrew396845@proton.me>
2026-02-09 12:17:24 +0300

    update stt.py add new featues and add timeout in listening

--- python/stt/stt.py
+++ python/stt/stt.py
@@ -3,29 +3,48 @@
 import json
 import alsaaudio
 from vosk import Model, KaldiRecognizer
+import os
 
-def listen(model_path: str):
-    SAMPLE_RATE = 16000
-    CHANNELS = 1
-    PERIOD_SIZE = 4000
+models_dict = {}
+def load_model(model_path: str, model_name: str):
+    global modeles_dict
 
-    model = Model(model_path)
-    recognizer = KaldiRecognizer(model, SAMPLE_RATE)
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(f"model: '{model_path}' not found.")
 
+    models_dict[model_name] = Model(model_path)
+
+def listen(model_name: str, timeout=0):
+    global models_dict
+    modifier = 0.25
+    if not timeout:
+        timeout = 1
+        modifier = 0
+
+    sample_rate = 16000
+    channels = 1
+    period_size = 4000
+
+    if model_name not in models_dict:
+        raise ValueError(f"model: '{model_name}' not loaded")
+
+    recognizer = KaldiRecognizer(models_dict[model_name], sample_rate)
+       
     pcm = alsaaudio.PCM(
         type=alsaaudio.PCM_CAPTURE,
         mode=alsaaudio.PCM_NORMAL
     )
 
-    pcm.setchannels(CHANNELS)
-    pcm.setrate(SAMPLE_RATE)
+    pcm.setchannels(channels)
+    pcm.setrate(sample_rate)
     pcm.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-    pcm.setperiodsize(PERIOD_SIZE)
+    pcm.setperiodsize(period_size)
 
-    print("Listening... Press Ctrl+C to stop.")
+    print("listening... press ctrl+c to stop.")
 
     try:
-        while True:
+        while timeout: 
+            timeout -= modifier
             length, data = pcm.read()
 
             if length == 0:
@@ -35,6 +54,7 @@ def listen(model_path: str):
                 result = json.loads(recognizer.Result())
                 text = result.get("text", "")
                 if text:
+                    pcm.close()
                     return text
 
     except KeyboardInterrupt:
