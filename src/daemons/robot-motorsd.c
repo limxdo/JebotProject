a087ad98eba842c90772721e1b86666426860f6a
limxdo <233496955+limxdo@users.noreply.github.com>
2026-02-09 14:37:50 +0300

    Add new Features and parsing function, improve replaying & logging, and more

--- src/daemons/robot-motorsd.c
+++ src/daemons/robot-motorsd.c
@@ -3,14 +3,17 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+/* FIFO files path */
 #define CMD_FIFO        "robot-cmd"
 #define REPLAY_FIFO     "robot-replay"
 
-int status = 0;
-int RUNNING = 1;
+unsigned int RUNNING = 1;
 
 /* handler of signals */
 void handler(int signum) {
@@ -18,31 +21,139 @@ void handler(int signum) {
     puts("");
 }
 
+
+/* enum command types */
+enum cmd_type {
+    CMD_MOVE_FORWARD,
+    CMD_MOVE_BACK,
+    CMD_TURN_RIGHT,
+    CMD_TURN_LEFT,
+    CMD_UNKNOWN
+};
+
+/* struct to store command & args from CMD_FIFO */
+struct cmd_args {
+    enum cmd_type type;
+    unsigned long seconds;
+    bool replay;
+};
+
+/* function to filter a struct from one string */
+void parse_cmd(struct cmd_args *cmds, const char *s) {
+
+    /* default values */
+    cmds->type = CMD_UNKNOWN;
+    cmds->seconds = 0;
+    cmds->replay = false;
+
+    char scopy[strlen(s)+1]; // +1 for '\0'
+    strcpy(scopy, s);
+
+    char *argptr;
+    char *endul;    // for strtoul
+
+    argptr = strtok(scopy, " ");
+
+    /* get type of cmds->type */
+    if (argptr == NULL) {
+        return;
+    }
+    else {
+        /* place values with ENUMs */
+        if (strcmp(argptr, "MOVE_FORWARD") == 0) {
+            cmds->type = CMD_MOVE_FORWARD;
+        }
+        else if (strcmp(argptr, "MOVE_BACK") == 0) {
+            cmds->type = CMD_MOVE_BACK;
+        }
+        else if (strcmp(argptr, "TURN_RIGHT") == 0) {
+            cmds->type = CMD_TURN_RIGHT;
+        }
+        else if (strcmp(argptr, "TURN_LEFT") == 0) {
+            cmds->type = CMD_TURN_LEFT;
+        }
+        else {
+            cmds->type = CMD_UNKNOWN;
+            return;
+        }
+    }
+
+    /* get seconds */
+    argptr = strtok(NULL, " ");
+    
+    if (argptr == NULL) {
+        return;
+    }
+    else {
+        if (strcmp(argptr, "--replay") == 0) {
+            cmds->replay = true;
+            return;
+        }
+        else {
+            cmds->seconds = strtoul(argptr, &endul, 10);
+            if (endul == argptr) {  // not a number
+                cmds->seconds = 0;
+            }
+        }
+    }
+    argptr = strtok(NULL, " ");
+
+    if (argptr == NULL) {
+        return;
+    }
+    else {
+        if (strcmp(argptr, "--replay") == 0) {
+            cmds->replay = true;
+        }
+    }
+}
+
 /* main function to moving */
-int move(const char *mode, size_t seconds) {
-    return 1;
+int move(enum cmd_type mode, unsigned long seconds) {
+    /* continue this function later */
+    return 1; // return true
+}
+
+/* replay function */
+void replay(int exec_status) {
+    /*
+     * exec_status can get it from move() function
+     */
+
+    int REPLAY_FIFO_FD = -1;
+    char msg[16];
+    strcpy(msg, exec_status ? "SUCCESS" : "FAILED");
+
+    printf("\033[32mEXECUTION:\033[0m (%s)\n", msg);
+
+    /* open REPLAY_FIFO */
+    if ((REPLAY_FIFO_FD = open(REPLAY_FIFO, O_WRONLY | O_NONBLOCK)) < 0) {
+        fprintf(stderr, "\033[31mREPLAY: ERROR\033[0m (%s)\n", strerror(errno));
+        return;
+    }
+    else {
+        if (write(REPLAY_FIFO_FD, msg, strlen(msg)) < 0) {
+            fprintf(stderr, "\033[31mREPLAY: WRITE ERROR\033[0m (%s)\n", strerror(errno));
+        } else {
+            printf("\033[32mREPLAY:\033[0m (%s)\n", msg);
+        }
+
+        close(REPLAY_FIFO_FD);
+    }
 }
 
 int main(void) {
 
     printf("PID: %d\n", getpid());
 
-    //int gpioinit = gpioInitialise();
-    int gpioinit = 1;
-
     /* signal handling */
     signal(SIGINT, handler);
     signal(SIGTERM, handler);
     signal(SIGPIPE, SIG_IGN);
 
-    if (gpioinit < 0) {
-        fprintf(stderr, "\033[31mFAILED to Initialise GPIO\033[0m\n");
-        return 1;
-    }
 
-
-    /* file descreptors */
-    int CMD_FIFO_FD = -1, REPLAY_FIFO_FD = -1;
+    /* robot-cmd FD */
+    int CMD_FIFO_FD = -1;
 
     /* creating FIFOs files */
     while (mkfifo(CMD_FIFO, 0666) < 0) {
@@ -50,7 +161,7 @@ int main(void) {
         perror("\033[31mWARNING: mmkfifo CMD_FIFO\033[0m");
         usleep(50000) ;
     }
-    printf("\033[32mCreating\033[0m '%s' \033[32mSuccess\033[0m\n", CMD_FIFO);
+    //printf("\033[32mCreating\033[0m '%s' \033[32mSuccess\033[0m\n", CMD_FIFO); // (verbose)
     chmod(CMD_FIFO, 0666);
 
     while ((CMD_FIFO_FD = open(CMD_FIFO, O_RDONLY | O_NONBLOCK)) < 0) {
@@ -65,17 +176,15 @@ int main(void) {
         usleep(50000) ;
     }
     chmod(REPLAY_FIFO, 0666);
-    printf("\033[32mCreating\033[0m '%s' \033[32mSuccess\033[0m\n", REPLAY_FIFO);
-
-    /* while ((REPLAY_FIFO_FD = open(REPLAY_FIFO, O_WRONLY | O_NONBLOCK)) < 0) {
-        perror("\033[31mWARNING: open CMD_FIFO_FD\033[0m");
-        usleep(10000);
-    } */
+    //printf("\033[32mCreating\033[0m '%s' \033[32mSuccess\033[0m\n", REPLAY_FIFO); // (verbose)
 
     /*******************************************************/
 
     char request[256];
-    ssize_t rn, wn;
+    ssize_t rn;
+    int exec_status = 0;
+
+    struct cmd_args commands;
 
     puts("\n|********** Daemon Is Started **********|\n");
     while(RUNNING) {
@@ -83,7 +192,64 @@ int main(void) {
         if (rn > 0) {
             request[rn] = '\0';
 
-            printf("\033[32mGETTED COMMAND:\033[0m %s\n", request);
+            printf("\033[32mRECEIVED COMMAND:\033[0m '%s'\n", request);
+
+            parse_cmd(&commands, request);
+
+            /* printf info ***(verbose)*** */
+            /* 
+            printf("type:           ");
+            switch (commands.type) {
+                case CMD_MOVE_FORWARD:
+                    printf("CMD_MOVE_FORWARD\n");
+                    break;
+                case CMD_MOVE_BACK:
+                    printf("CMD_MOVE_BACK\n");
+                    break;
+                case CMD_TURN_RIGHT:
+                    printf("CMD_TURN_RIGHT\n");
+                    break;
+                case CMD_TURN_LEFT:
+                    printf("CMD_TURN_LEFT\n");
+                    break;
+                case CMD_UNKNOWN:
+                    printf("CMD_UNKNOWN\n");
+                    break;
+            }
+            printf("seconds:        %lu\n", commands.seconds);
+            printf("replay:         %d\n", commands.replay);
+            */
+
+            /* checking */
+            if (commands.type == CMD_UNKNOWN) {
+                fprintf(stderr, "\033[31mERORR: INVALID COMMAND\033[0m\n");
+                continue;
+            }
+            else if ((commands.type == CMD_MOVE_FORWARD || commands.type == CMD_MOVE_BACK) && !commands.seconds) {
+                fprintf(stderr, "\033[31mERORR: MISSING SECONDS\033[0m\n");
+                continue;
+            }
+            else if ((commands.type == CMD_TURN_RIGHT || commands.type == CMD_TURN_LEFT) && commands.seconds) {
+                fprintf(stderr, "\033[31mERORR: INVALID OPTIONS\033[0m\n");
+                continue;
+            }
+            else {
+                if (commands.type == CMD_TURN_RIGHT)
+                    exec_status = move(CMD_TURN_RIGHT, 0);
+
+                else if (commands.type == CMD_TURN_LEFT)
+                    exec_status = move(CMD_TURN_LEFT, 0);
+
+                else if (commands.type == CMD_MOVE_FORWARD)
+                    exec_status = move(CMD_MOVE_FORWARD, commands.seconds);
+
+                else if (commands.type == CMD_MOVE_BACK)
+                    exec_status = move(CMD_MOVE_BACK, commands.seconds);
+
+                if (commands.replay) {
+                    replay(exec_status);
+                }
+            }
         }
 
         usleep(1);
@@ -98,9 +264,6 @@ int main(void) {
     if (CMD_FIFO_FD > 0)
         close(CMD_FIFO_FD);
 
-    if (REPLAY_FIFO_FD > 0)
-        close(CMD_FIFO_FD);
-
     /* remove FIFO files */
     if (unlink(CMD_FIFO) < 0)
         perror("\033[31mWARNING: unlink CMD_FIFO\033[0m");
@@ -111,5 +274,5 @@ int main(void) {
 
     printf("\nexiting...\n");
 
-    return status;
+    return 0;
 }
