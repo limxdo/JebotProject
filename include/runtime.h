db9960c218b678638a2e99f03b3e80535f43359e
limxdo <233496955+limxdo@users.noreply.github.com>
2026-04-14 14:56:21 +0300

    add runtime module to manage runtime paths

---
+++ include/runtime.h
@@ -0,0 +1,14 @@
+#ifndef RUNTIME_H
+#define RUNTIME_H
+
+#include <fcntl.h>
+
+#define RUNTIME_PATH "/run/jebot"
+
+int runtime_init(const char *base, mode_t mode);
+int runtime_mkdir(const char *name, mode_t mode);
+int runtime_open(const char *path, int flags, mode_t mode);
+int runtime_pid(pid_t pid);
+void runtime_exit(void);
+
+#endif
