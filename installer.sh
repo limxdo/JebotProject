1a5c2d79847fd44181181382789a6b6505087f9e
limxdo <233496955+limxdo@users.noreply.github.com>
2026-04-25 15:36:29 +0300

    add first installer

---
+++ installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+USAGE="usage:\n\t$0 [install|uninstall]"
+
+PREFIX="/usr/local"
+LIBEXECDIR="$PREFIX/libexec/jebot"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "this script must be run as root." >&2
+    exit 1
+fi
+
+case "$1" in
+    install)
+        mkdir -p "$LIBEXECDIR"
+        cp bin/* "$LIBEXECDIR"/
+        chmod +x "$LIBEXECDIR"/*
+        ;;
+    uninstall)
+        rm -rf "$LIBEXECDIR"
+        ;;
+    *)
+        echo -e "$USAGE" >&2
+        exit 2
+        ;;
+esac
