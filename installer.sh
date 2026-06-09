2f16c1577bded524119c10235fd0ad2302c7cda6
limxdo <233496955+limxdo@users.noreply.github.com>
2026-06-10 01:14:33 +0300

    add systemd service files

--- installer.sh
+++ installer.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-USAGE="usage:\n\t$0 [install|uninstall]"
-
-PREFIX="/usr/local"
-LIBEXECDIR="$PREFIX/libexec/jebot"
+LIBEXEC_DIR="/usr/local/libexec/jebot"
+SYSTEMD_DIR="/etc/systemd/system"
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "this script must be run as root." >&2
@@ -14,15 +12,27 @@ fi
 
 case "$1" in
     install)
-        mkdir -p "$LIBEXECDIR"
-        cp -f bin/* "$LIBEXECDIR"/
-        chmod +x "$LIBEXECDIR"/*
+        mkdir -p "$LIBEXEC_DIR"
+        cp -f bin/* "$LIBEXEC_DIR"/
+        chmod +x "$LIBEXEC_DIR"/*
+        
+        cp -f systemd/jebot-*.service "$SYSTEMD_DIR"/
+        systemctl daemon-reload
+        for service in "$SYSTEMD_DIR"/jebot-*.service; do
+            systemctl enable "$(basename $service)"
+        done
         ;;
     uninstall)
-        rm -rf "$LIBEXECDIR"
+        rm -rf "$LIBEXEC_DIR"
+
+        for service in "$SYSTEMD_DIR"/jebot-*.service; do
+            systemctl disable --now "$(basename $service)"
+        done
+        rm -f "$SYSTEMD_DIR"/jebot-*.service
+        systemctl daemon-reload
         ;;
     *)
-        echo -e "$USAGE" >&2
+        echo -e "usage:\n\t$0 [install|uninstall]" >&2
         exit 2
         ;;
 esac
